c243f61c681d0490f2f1ad9b6d5276ac2c2ec6ea
Zongle Wang <wangzongler@gmail.com>
2026-03-05 20:31:58 +0800

    Add Develocity plugin and build scan configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,3 +14,14 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+plugins { id("com.gradle.develocity") version "4.3.2" }
+
+develocity {
+  buildScan {
+    termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
+    termsOfUseAgree = "yes"
+    val isCI = providers.environmentVariable("CI").isPresent
+    publishing.onlyIf { isCI }
+  }
+}
