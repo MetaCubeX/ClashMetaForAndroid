8ff4ed326a4d1169d574b6eb61119790e5886887
Nemu-x <fent95@gmail.com>
2026-04-05 18:07:58 +0300

    refactor(common): update component helpers and constants

--- common/src/main/java/com/github/kr328/clash/common/constants/Components.kt
+++ common/src/main/java/com/github/kr328/clash/common/constants/Components.kt
@@ -7,5 +7,7 @@ object Components {
     private const val componentsPackageName = "com.github.kr328.clash"
 
     val MAIN_ACTIVITY = ComponentName(packageName, "$componentsPackageName.MainActivity")
+    /** Launcher alias from manifest (`activity-alias android:name=".MainActivityAlias"`). */
+    val MAIN_ACTIVITY_ALIAS = ComponentName(packageName, "$componentsPackageName.MainActivityAlias")
     val PROPERTIES_ACTIVITY = ComponentName(packageName, "$componentsPackageName.PropertiesActivity")
 }
