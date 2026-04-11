b3d1aca1ee7d53b87fcfb1138f03d50b569b2a20
Nemu-x <fent95@gmail.com>
2026-04-11 22:33:43 +0300

    chore: release 0.3.5 (211030)

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,8 +60,8 @@ subprojects {
             minSdk = 21
             targetSdk = 35
 
-            versionName = "0.3.4"
-            versionCode = 211029
+            versionName = "0.3.5"
+            versionCode = 211030
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
