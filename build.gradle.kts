8851824470bcffd0ba5111a40f35e40457696839
Nemu-x <fent95@gmail.com>
2026-04-26 22:02:07 +0300

    chore(release): bump version to 0.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,8 +60,8 @@ subprojects {
             minSdk = 21
             targetSdk = 35
 
-            versionName = "0.4.2"
-            versionCode = 402000
+            versionName = "0.5.0"
+            versionCode = 500000
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
